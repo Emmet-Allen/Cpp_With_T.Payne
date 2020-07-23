0fbe49e02906c20c4bfea98cccbf73ceec8961a6
Emmet Allen <59685332+Mrskillful@users.noreply.github.com>
2020-07-23 09:38:07 -0400

    Added 01_myfirstprogram
    T.Payne First C++ video along with notes

---
+++ 01_myfirstprogram_/01_myfirstprogram_/Challenge1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int challenge1() {
+	std::cout << "I'm going to be the best programmer in the world!";
+		return 0;
+}
---
+++ 01_myfirstprogram_/01_myfirstprogram_/main.cpp
@@ -0,0 +1,21 @@
+//This is my first C++ Program! = // to Comment out line
+
+/*#include = import the following...
+<iostream> = file to import, custom files should have quotes
+instead of <> */
+#include <iostream>
+
+/*int main(){ = Function return type, function name, arguments in (), 
+then enclose function in Braces {} */
+int main() {
+
+/*std::cout << = Format for output in C++, same as 'print' in python
+"Ham Sandwich!" = string to be output, end every line with ";"
+<< = Operator that tells what is being outputed 
+C++ is praticular, use "" for strings and '' used for exclusively single characters */
+	std::cout << "Ham Sandwich!\n";
+
+/*return 0; = function returns an int, so that the functions 
+output of int is satisfied */
+	return 0;
+}
